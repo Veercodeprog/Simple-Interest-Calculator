e8ee955418c1cb1df10718dbe642d624e12747a3
Veer Pratap <91560462+Veercodeprog@users.noreply.github.com>
2021-11-01 16:43:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ function sioutput()
 
     var simpleInt = parseFloat(principal.value) *(1 + parseFloat(rate.value) * parseInt(years.value) /100);
     simpleInt = simpleInt.toFixed(2);
-    
+    if(principal.value==0){
+      alert("please enter a positive value of Principal");
+    }
+   
    
     Years = 2021+parseInt(years.value);
     document.getElementById("siOutput-01").innerHTML = "If you deposit today Rs."+"<mark>"+principal.value+"</mark>"+"<br>"+"at an interest rate of "+"<mark>"+rate.value+"%."+"</mark>"+"<br>"+"You will receive an amount of Rs."+"<mark>"+simpleInt+"</mark>"+","+"<br>"+"in the year "+"<mark>"+Years+"</mark>"+"."
